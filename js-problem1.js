6a5607f9e7ef6b1c27765b9da5af97954e47023f
MdAlAmin212104 <mdalamin212104@gmail.com>
2024-02-04 18:00:06 +0600

    first problem solved

--- js-problem1.js
+++ js-problem1.js
@@ -3,7 +3,11 @@
 Write a function to convert temperature from Celsius to Fahrenheit.
  °F = °C×(9/5)+32
 */
-
+function celsiusIntoFahrenheit(celsius){
+      const fahrenheit = celsius*(9/5)+32;
+      return fahrenheit;
+}
+console.log(celsiusIntoFahrenheit(32));
 
 
 
